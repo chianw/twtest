58bf1a51bee1392a3a8eafdd6ec415d698fbbc5c
chianw <chiancw@gmail.com>
2025-09-12 13:59:31 +0800

    allow public access

--- sa.tf
+++ sa.tf
@@ -8,12 +8,13 @@ resource "azurerm_role_assignment" "example" {
 
 
 module "avm-res-storage-storageaccount" {
-  source                    = "Azure/avm-res-storage-storageaccount/azurerm"
-  version                   = "0.6.4"
-  location                  = azurerm_resource_group.this_rg.location
-  resource_group_name       = azurerm_resource_group.this_rg.name
-  name                      = module.naming.storage_account.name_unique
-  account_replication_type  = "LRS"
-  shared_access_key_enabled = true
-  depends_on = [ azurerm_role_assignment.example ]
+  source                        = "Azure/avm-res-storage-storageaccount/azurerm"
+  version                       = "0.6.4"
+  location                      = azurerm_resource_group.this_rg.location
+  resource_group_name           = azurerm_resource_group.this_rg.name
+  name                          = module.naming.storage_account.name_unique
+  account_replication_type      = "LRS"
+  shared_access_key_enabled     = true
+  public_network_access_enabled = true
+  depends_on                    = [azurerm_role_assignment.example]
 }
