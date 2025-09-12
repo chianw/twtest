8336e630e04fc141c2b4e263a3107491625c95ac
chianw <chiancw@gmail.com>
2025-09-12 11:55:13 +0800

    use LRS

--- sa.tf
+++ sa.tf
@@ -4,5 +4,5 @@ module "avm-res-storage-storageaccount" {
   location                 = azurerm_resource_group.this_rg.location
   resource_group_name      = azurerm_resource_group.this_rg.name
   name                     = module.naming.storage_account.name_unique
-  account_replication_type = "ZRS"
+  account_replication_type = "LRS"
 }
