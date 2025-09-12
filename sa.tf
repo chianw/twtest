648f313446f99d75d1b0f27c5b6aa21358ae8c10
chianw <chiancw@gmail.com>
2025-09-12 13:07:27 +0800

    enable shared key

--- sa.tf
+++ sa.tf
@@ -1,8 +1,9 @@
 module "avm-res-storage-storageaccount" {
-  source                   = "Azure/avm-res-storage-storageaccount/azurerm"
-  version                  = "0.6.4"
-  location                 = azurerm_resource_group.this_rg.location
-  resource_group_name      = azurerm_resource_group.this_rg.name
-  name                     = module.naming.storage_account.name_unique
-  account_replication_type = "LRS"
+  source                    = "Azure/avm-res-storage-storageaccount/azurerm"
+  version                   = "0.6.4"
+  location                  = azurerm_resource_group.this_rg.location
+  resource_group_name       = azurerm_resource_group.this_rg.name
+  name                      = module.naming.storage_account.name_unique
+  account_replication_type  = "LRS"
+  shared_access_key_enabled = true
 }
