dc5f3f1d48b8f5d5c7bda9c60b3d48c4c365c66d
chianw <chiancw@gmail.com>
2025-09-15 11:39:31 +0800

    update vmss example

--- vmss/vmss.tf
+++ vmss/vmss.tf
@@ -1,35 +1,126 @@
+# This ensures we have unique CAF compliant names for our resources.
 module "naming" {
   source  = "Azure/naming/azurerm"
-  version = "~> 0.4"
+  version = "0.4.1"
 }
 
 
-resource "azurerm_resource_group" "this_rg" {
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
   location = "taiwannorth"
   name     = module.naming.resource_group.name_unique
+  tags     = local.tags
 }
 
-module "avm-res-compute-virtualmachinescaleset" {
+resource "azurerm_virtual_network" "this" {
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.virtual_network.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  address_space       = ["10.0.0.0/16"]
+  tags                = local.tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  address_prefixes     = ["10.0.1.0/24"]
+  name                 = module.naming.subnet.name_unique
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+}
+
+# network security group for the nic with a rule to allow http traffic
+resource "azurerm_network_security_group" "nic" {
+  location            = azurerm_resource_group.this.location
+  name                = "${module.naming.network_security_group.name_unique}-nic"
+  resource_group_name = azurerm_resource_group.this.name
+
+  security_rule {
+    access                     = "Allow"
+    destination_address_prefix = "*"
+    destination_port_range     = "80"
+    direction                  = "Inbound"
+    name                       = "allow-http"
+    priority                   = 100
+    protocol                   = "Tcp"
+    source_address_prefix      = "*"
+    source_port_range          = "*"
+  }
+}
+
+# network security group for the subnet with a rule to allow http traffic
+resource "azurerm_network_security_group" "subnet" {
+  location            = azurerm_resource_group.this.location
+  name                = "${module.naming.network_security_group.name_unique}-subnet"
+  resource_group_name = azurerm_resource_group.this.name
+
+  security_rule {
+    access                     = "Allow"
+    destination_address_prefix = "*"
+    destination_port_range     = "80"
+    direction                  = "Inbound"
+    name                       = "allow-http"
+    priority                   = 100
+    protocol                   = "Tcp"
+    source_address_prefix      = "*"
+    source_port_range          = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "this" {
+  network_security_group_id = azurerm_network_security_group.subnet.id
+  subnet_id                 = azurerm_subnet.subnet.id
+}
+
+
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# This is the module call
+module "terraform_azurerm_avm_res_compute_virtualmachinescaleset" {
   source                      = "Azure/avm-res-compute-virtualmachinescaleset/azurerm"
   version                     = "0.8.0"
-  location                    = "taiwannorth"
-  name                        = "twvmss1"
-  resource_group_name         = azurerm_resource_group.this_rg.name
   extension_protected_setting = {}
-  user_data_base64            = null
-  enable_telemetry            = false
+  location                    = azurerm_resource_group.this.location
+  # source             = "Azure/avm-res-compute-virtualmachinescaleset/azurerm"
+  name                = module.naming.virtual_machine_scale_set.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  user_data_base64    = null
+  admin_password      = "P@ssw0rd1234!"
+  admin_ssh_keys = [(
+    {
+      id         = tls_private_key.example_ssh.id
+      public_key = tls_private_key.example_ssh.public_key_openssh
+      username   = "azureuser"
+    }
+  )]
 
+  enable_telemetry = false
+  extension = [{
+    name                        = "HealthExtension"
+    publisher                   = "Microsoft.ManagedServices"
+    type                        = "ApplicationHealthLinux"
+    type_handler_version        = "1.0"
+    auto_upgrade_minor_version  = true
+    failure_suppression_enabled = false
+    settings                    = "{\"port\":80,\"protocol\":\"http\",\"requestPath\":\"/index.html\"}"
+  }]
+  instances = 1
+  network_interface = [{
+    name                      = "VMSS-NIC"
+    network_security_group_id = azurerm_network_security_group.nic.id
+    ip_configuration = [{
+      name      = "VMSS-IPConfig"
+      subnet_id = azurerm_subnet.subnet.id
+    }]
+  }]
   os_profile = {
     linux_configuration = {
-      disable_password_authentication = true
+      disable_password_authentication = false
       admin_username                  = "azureuser"
-      patch_mode                      = "ImageDefault"
+      admin_ssh_key                   = toset([tls_private_key.example_ssh.id])
     }
   }
-  automatic_instance_repair = {
-    enabled = false
-  }
-
   sku_name = "Standard_B2ms"
   source_image_reference = {
     publisher = "Canonical"
@@ -37,5 +128,13 @@ module "avm-res-compute-virtualmachinescaleset" {
     sku       = "22_04-LTS-gen2" # Auto guest patching is enabled on this sku.  https://learn.microsoft.com/en-us/azure/virtual-machines/automatic-vm-guest-patching
     version   = "latest"
   }
+  tags = local.tags
 
+  # Uncomment the code below to implement a VMSS Lock
+  #lock = {
+  #  name = "VMSSNoDelete"
+  #  kind = "CanNotDelete"
+  #}
 }
+
+
