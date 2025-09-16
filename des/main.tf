35127d7d752e223ae4e866b7944ad5f813b3aab6
chianw <chiancw@gmail.com>
2025-09-16 16:05:05 +0800

    update kv roles

--- des/main.tf
+++ des/main.tf
@@ -13,46 +13,65 @@ resource "azurerm_resource_group" "this" {
 }
 
 
-module "keyvault" {
+module "avm_res_keyvault_vault" {
   source  = "Azure/avm-res-keyvault-vault/azurerm"
-  version = "0.9.1"
+  version = "=0.10.0"
 
-  location                    = azurerm_resource_group.this.location
-  name                        = module.naming.key_vault.name_unique
-  resource_group_name         = azurerm_resource_group.this.name
+  location                    = azurerm_resource_group.this_rg.location
+  name                        = "${module.naming.key_vault.name_unique}-linux-ssh"
+  resource_group_name         = azurerm_resource_group.this_rg.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   enabled_for_disk_encryption = true
-  network_acls = {
-    bypass                     = "AzureServices"
-    default_action             = "Allow"
-    ip_rules                   = []
-    virtual_network_subnet_ids = []
-  }
-  purge_protection_enabled = false
-  sku_name                 = "standard"
-}
+  keys = {
+    des_key = {
+      name     = "des-disk-key"
+      key_type = "RSA"
+      key_size = 2048
 
-resource "azurerm_key_vault_key" "example" {
-  key_opts = [
-    "decrypt",
-    "encrypt",
-    "sign",
-    "unwrapKey",
-    "verify",
-    "wrapKey",
-  ]
-  key_type     = "RSA"
-  key_vault_id = module.keyvault.resource_id
-  name         = "des-example-key"
-  key_size     = 2048
+      key_opts = [
+        "decrypt",
+        "encrypt",
+        "sign",
+        "unwrapKey",
+        "verify",
+        "wrapKey",
+      ]
+    }
+  }
+  network_acls = {
+    default_action = "Allow"
+    bypass         = "AzureServices"
+  }
+  role_assignments = {
+    deployment_user_secrets = { #give the deployment user access to secrets
+      role_definition_id_or_name = "Key Vault Secrets Officer"
+      principal_id               = data.azurerm_client_config.current.object_id
+    }
+    deployment_user_keys = { #give the deployment user access to keys
+      role_definition_id_or_name = "Key Vault Crypto Officer"
+      principal_id               = data.azurerm_client_config.current.object_id
+    }
+    user_managed_identity_keys = { #give the user assigned managed identity for the disk encryption set access to keys
+      role_definition_id_or_name = "Key Vault Crypto Officer"
+      principal_id               = azurerm_user_assigned_identity.example_identity.principal_id
+      principal_type             = "ServicePrincipal"
+    }
+  }
+
+  wait_for_rbac_before_key_operations = {
+    create = "60s"
+  }
+  wait_for_rbac_before_secret_operations = {
+    create = "60s"
+  }
 }
 
 module "des" {
   source  = "Azure/avm-res-compute-diskencryptionset/azurerm"
   version = "0.1.0"
 
-  key_vault_key_id      = azurerm_key_vault_key.example.id
-  key_vault_resource_id = module.keyvault.resource_id
+  key_vault_key_id      = module.avm_res_keyvault_vault.keys_resource_ids.des_key.id
+  key_vault_resource_id = module.avm_res_keyvault_vault.resource_id
   location              = azurerm_resource_group.this.location
   name                  = module.naming.disk_encryption_set.name_unique
   resource_group_name   = azurerm_resource_group.this.name
